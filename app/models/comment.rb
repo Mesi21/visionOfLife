f1a4180f2ff1c575318e6843ced93d1e7de1dc2c
Mesi <mse212002@gmail.com>
2023-12-09 07:59:23 +0200

    :sparkles: add method for update the comments counter update_counter_for_comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
     validates :text, presence: true
     belongs_to :user
     belongs_to :post
+
+    private
+
+    def update_counter_for_comments
+        post.increment!(:comments_counter)
+    end
 end
