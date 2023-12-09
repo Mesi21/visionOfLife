fbf68b85bd20a3964291b2b88c564a94d476e45c
Mesi <mse212002@gmail.com>
2023-12-09 07:56:09 +0200

    :sparkles: add methods for post for updating update_post_counter and displaying most_recent_five_comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :likes
+    after_save :update_post_counter
+
+    private
+
+    def update_post_counter
+        user.increment!(:posts_counter)
+    end
+
+    def most_recent_five_comments
+        comments.order('created_at DESC').last(5)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User  < ApplicationRecord
     validates :name, presence: true
     has_many :posts
 
-    def most_recent_posts
+    def most_recent_three_posts
         post.order('created_at DESC').last(3)
     end
 end
