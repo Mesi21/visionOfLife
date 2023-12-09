97dedcf9854567af3df88510f162b45bfe544fc2
Mesi <mse212002@gmail.com>
2023-12-09 08:01:24 +0200

    :sparkles: add method for update the likes counter update_counter_for_likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like <ApplicationRecord
     belongs_to :user
     belongs_to :post
+    after_save :update_counter_for_likes
+
+    private
+
+    def update_counter_for_likes
+        post.increment(:likes_counter)
+    end
 end
