300bcea4993ace07aa1d634dcaee795f05b1e99c
Mesi <mse212002@gmail.com>
2023-12-13 11:56:43 +0200

    :white_check_mark: add tests for the users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users controller testing', type: :request do
+  describe 'GET /index users' do
+    it 'should render the index page with a full list of users and render the index template' do
+      get '/users'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+      expect(response.body).to include('placeholder txt')
+    end
+
+    it 'renders the show page with info about a specific user and the corresponding template' do
+      get '/users/2'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('show')
+      expect(response.body).to include('info')
+    end
   end
 end
