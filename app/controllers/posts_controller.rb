a01d44c1f033a4b8c500221f7ea882ce52d68321
Mesi <mse212002@gmail.com>
2023-12-16 23:32:04 +0200

    :construction: implement code reviewer's suggestions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @user = @post.user
     @comments = @post.comments.order(created_at: :desc)
     @likes = @post.likes.all
   end
@@ -18,14 +18,10 @@ class PostsController < ApplicationController
   def create
     new_post = current_user.posts.build(post_params)
 
-    respond_to do |format|
-      format.html do
-        if new_post.save
-          redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post was successfully created.'
-        else
-          render :new, alert: 'Post was not created.'
-        end
-      end
+    if new_post.save
+      redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post was successfully created.'
+    else
+      render :new, alert: 'Post was not created.'
     end
   end
 
